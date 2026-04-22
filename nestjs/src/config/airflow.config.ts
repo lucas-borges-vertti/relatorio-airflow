f0f6a067054cdcff0f22b247d9ed8c50aaff9487
Lucas Borges <lucas.borges@vertti.com.br>
2026-04-22 09:04:52 -0300

    feat: remove nest

--- nestjs/src/config/airflow.config.ts
+++
@@ -1,6 +0,0 @@
-export const airflowConfig = {
-  baseUrl: process.env.AIRFLOW_BASE_URL || 'http://localhost:8080',
-  dagId: process.env.AIRFLOW_DAG_ID || 'report_generation_dag',
-  apiUser: process.env.AIRFLOW_API_USER || 'airflow',
-  apiPassword: process.env.AIRFLOW_API_PASSWORD || 'airflow',
-};
